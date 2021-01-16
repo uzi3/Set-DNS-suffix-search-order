6c9a073de48fd26667edc6c600e1ebc8b6391c92
uzi3 <43525327+uzi3@users.noreply.github.com>
2021-01-16 10:01:36 +0530

    first commit

---
+++ DNSSuffixSearchOrder.ps1
@@ -0,0 +1,5 @@
+﻿$Machines=Get-Content D:\MachineList.txt
+foreach ($i in $Machines)
+{
+ Invoke-CimMethod -ClassName win32_networkadapterconfiguration -MethodName "SetDNSSuffixSearchOrder" -Arguments @{DNSDomainSuffixSearchOrder="sales.contoso.com", "contoso.com", "support.contoso.com", "marketing.contoso.com"}
+}
